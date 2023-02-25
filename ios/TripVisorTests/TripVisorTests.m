28a0b391f4a1cb897e184a4472746cfd227b9c30
Ansh <ansh.verma22@outlook.com>
2023-02-25 14:35:45 -0600

    Rename App Package to TripVisor

--- ios/TripVisorTests/TripVisorTests.m
+++ ios/TripVisorTests/TripVisorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeStarterKitTests : XCTestCase
+@interface TripVisorTests : XCTestCase
 
 @end
 
-@implementation ReactNativeStarterKitTests
+@implementation TripVisorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
