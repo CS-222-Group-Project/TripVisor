fcfa2b141696b75d232d11904ee77c89bb7f66ac
anandani4136 <ra7353@hotmail.com>
2023-04-10 23:36:09 -0500

    :art: Visual Sign In overhaul

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import {
-  StyleSheet, Text, View, Button,
+  StyleSheet, Text, View, Button, Image, useColorScheme,
 } from 'react-native';
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
+import CAR_IMAGE from './assets/TravelCar.png';
+import GOOG_IMAGE from './assets/google-logo.png';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
   const [token, setToken] = useState<string>('');
   const [userInfo, setUserInfo] = useState<any>(null);
 
@@ -47,15 +53,29 @@ export default function App(): JSX.Element {
   };
 
   return (
-    <View style={styles.container}>
+
+    <View style={[
+      styles.container,
+      {
+        backgroundColor: isDarkMode ? '#55596D' : '#D6DAEA',
+      },
+    ]}
+    >
       {userInfo === null ? (
-        <Button
-          title="Sign in with Google"
-          disabled={!request}
-          onPress={() => {
-            promptAsync();
-          }}
-        />
+
+        <View style={styles.box}>
+          <Text style={styles.title}>Sign in with</Text>
+          <Image source={CAR_IMAGE} style={styles.car} />
+          <Image source={GOOG_IMAGE} style={styles.google} />
+          <Button
+            title=""
+            disabled={!request}
+            onPress={() => {
+              promptAsync();
+            }}
+          />
+
+        </View>
       ) : (
         <Text style={styles.text}>{userInfo.name}</Text>
       )}
@@ -66,7 +86,6 @@ export default function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -74,4 +93,85 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  title: {
+    position: 'absolute',
+    textAlign: 'center',
+    width: 350,
+    fontSize: 60,
+    // marginLeft: '10%',
+    marginTop: '-60%',
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  box: {
+    backgroundColor: '#fff',
+    width: 350,
+    height: 250,
+    borderRadius: '25',
+    textAlign: 'center',
+    alignContent: 'center',
+    paddingTop: '25%',
+  },
+  car: {
+    position: 'absolute',
+    width: 380,
+    height: 395,
+    marginLeft: -12,
+  },
+  google: {
+    position: 'absolute',
+    width: 200,
+    height: 65,
+    marginLeft: 76,
+    marginTop: 82,
+  },
 });
+
+// function App(): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar
+//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//         backgroundColor={backgroundStyle.backgroundColor}
+//       />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}
+//       >
+//         <Header />
+//         <View
+//           style={{
+//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//           }}
+//         >
+//           <Section title="Step One">
+//             Edit
+//             {' '}
+//             <Text style={styles.highlight}>App.tsx</Text>
+//             {' '}
+//             to change this
+//             screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
+
+// export default App;
